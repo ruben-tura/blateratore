6d8f833edacd544cca974cc4adcc9e1741b64616
Ruben Tura <rubentura@proton.me>
2023-11-11 18:51:23 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,48 @@
+console.log("test");
+
+window.onload = function () {
+    const up = 38;
+    const down = 40;
+
+    window.onkeydown = function (key) {
+        if (key.keyCode === up) {
+            const categories = document.getElementsByClassName("cat-btn");
+            let toSelect = false;
+            let selected = false;
+            for (let i = categories.length - 1; i >= 0; i--) {
+                if (toSelect) {
+                    categories[i].classList.add("selected");
+                    selected = true;
+                    break;
+                }
+                toSelect = categories[i].classList.contains("selected");
+                if (toSelect) {
+                    categories[i].classList.remove("selected");
+                }
+            }
+            if (!selected) {
+                categories[categories.length - 1].classList.add("selected");
+            }
+        };
+        if (key.keyCode === down) {
+            const categories = document.getElementsByClassName("cat-btn");
+            let toSelect = false;
+            let selected = false;
+            for (let cat of categories) {
+                if (toSelect) {
+                    cat.classList.add("selected");
+                    selected = true;
+                    break;
+                }
+                toSelect = cat.classList.contains("selected");
+                if (toSelect) {
+                    cat.classList.remove("selected");
+                }
+            }
+            if (!selected) {
+                categories[0].classList.add("selected");
+            }
+        };
+    };
+
+}
