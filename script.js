9430806c0f976adbf3f663fcb46a2be28dd98017
Ruben Tura <rubentura@proton.me>
2023-11-12 09:31:57 +0100

    Added selection of first options when category selected

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 window.onload = function () {
+    const left = 37;
     const up = 38;
+    const right = 39;
     const down = 40;
 
     window.onkeydown = function (key) {
+
+        // Category scrolling
         if (key.keyCode === up) {
             const categories = document.getElementsByClassName("category");
             let toSelect = false;
@@ -16,12 +20,20 @@ window.onload = function () {
                 toSelect = categories[i].classList.contains("selected");
                 if (toSelect) {
                     categories[i].classList.remove("selected");
+                    const currentCatID = categories[i].id;
+                    const previousOptions = document.querySelectorAll("#" + currentCatID + " > .sub-menu > button");
+                    for (let option of previousOptions) {
+                        option.classList.remove("option-selected");
+                    }
                 }
             }
             if (!selected) {
                 categories[categories.length - 1].classList.add("selected");
             }
+            const options = document.querySelectorAll(".selected > .sub-menu > button");
+            options[0].classList.add("option-selected");
         };
+
         if (key.keyCode === down) {
             const categories = document.getElementsByClassName("category");
             let toSelect = false;
@@ -35,12 +47,21 @@ window.onload = function () {
                 toSelect = cat.classList.contains("selected");
                 if (toSelect) {
                     cat.classList.remove("selected");
+                    const currentCatID = cat.id;
+                    const previousOptions = document.querySelectorAll("#" + currentCatID + " > .sub-menu > button");
+                    for (let option of previousOptions) {
+                        option.classList.remove("option-selected");
+                    }
                 }
             }
             if (!selected) {
                 categories[0].classList.add("selected");
             }
+            const options = document.querySelectorAll(".selected > .sub-menu > button");
+            options[0].classList.add("option-selected");
         };
+
+
     };
 
 }
